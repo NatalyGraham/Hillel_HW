9b1dff1d0b0be71442e479cb4c719de4fc91f486
NatalyGraham <natalygraham2606@gmail.com>
2020-07-15 14:05:48 +0300

    added HW01

---
+++ HW01/src/Task3.java
@@ -0,0 +1,19 @@
+public class Task3 {
+	
+	public static void main (String[] args) {
+	
+		int[] array = {1, 0, 84, 298, 57, -65, 89, -2832};
+		
+		for (int i = 0; i < array.length; i++){
+			System.out.println(array [i]);
+		}
+		
+		System.out.println();
+	
+		for (int i = 0; i < array.length; i++){
+			System.out.print(array [i] + " ");
+		}
+		
+	}
+	
+}
