7022a8aa218c22d76d38a2e3c5b80372c323f293
Nataly Haievska <natalygraham2606@gmail.com>
2020-07-20 14:52:31 +0300

    reduced iterrations to find the Primer in HW03Task2

--- HW03/src/HW03Task2.java
+++ HW03/src/HW03Task2.java
@@ -1,20 +1,24 @@
 public class HW03Task2 {
     public static void main(String[] args) {
-        int[] initialArray = {45, 68, 532, 535, 337, 865, 461, 4985, 492, 795, 3, 546, 3571, 7};
+        int[] initialArray = {45, 68, 532, 535, 337, 865, 461, 4985, 492, 795, 1, 546, 3571, 7};
         int counter = 2;
+        //System.out.println((int) (Math.ceil(Math.sqrt(337))));
 
         for (int currentNumber : initialArray
         ) {
-            while (currentNumber % counter != 0 || counter == Math.sqrt(currentNumber)) {
-                counter++;
-            }
-            if (counter == currentNumber || currentNumber == 1) {
+            if (currentNumber == 1) {
                 System.out.print(currentNumber + " ");
+            } else {
+                while (currentNumber % counter != 0 && counter != (int) (Math.ceil(Math.sqrt(currentNumber)))) {
+                    counter++;
+                }
+                if (counter == (int) (Math.ceil(Math.sqrt(currentNumber))) || currentNumber == 1) {
+                    System.out.print(currentNumber + " ");
+                }
+                counter = 2;
             }
-            counter = 2;
         }
 
     }
 
 }
-
---
+++ HW04/src/HW04Task4.java
@@ -0,0 +1,8 @@
+package PACKAGE_NAME;
+
+/**
+ * @author Nataly Haievska
+ * @date 20.07.2020
+ */
+public class HW04Task4 {
+}
