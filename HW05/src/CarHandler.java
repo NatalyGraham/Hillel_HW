4f7830be9e23eb1e51676b2639c5f391fdff0ca1
NatalyGraham <natalygraham2606@gmail.com>
2020-07-21 14:28:55 +0300

    added HW05

---
+++ HW05/src/CarHandler.java
@@ -0,0 +1,29 @@
+public class CarHandler {
+
+    public static void main(String[] args) {
+
+        Car[] cars = new Car[10];
+        cars[0] = new Car("id123",
+                "Mersedes",
+                "S500",
+                2017,
+                "black",
+                78999.99,
+                "AX0000XA" );
+
+    }
+
+/*    public void carCreator() {
+        Car[] cars = new Car[20];
+        cars[0] = new Car("id123",
+                    "Mersedes",
+                "S500",
+                2017,
+                "black",
+                78999.99,
+                "AX0000XA" );
+    }//*/
+    public void carBrandSearcher (Car[] cars, String brand) {
+
+    }
+}
