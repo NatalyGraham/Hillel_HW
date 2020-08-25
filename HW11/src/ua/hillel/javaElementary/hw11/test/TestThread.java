8280717f1b36e7dccfcc917d9c098e3af60cea6c
Nataly Graham <natalygraham2606@gmail.com>
2020-08-25 07:38:46 +0300

    hw11 test

---
+++ HW11/src/ua/hillel/javaElementary/hw11/test/TestThread.java
@@ -0,0 +1,9 @@
+package ua.hillel.javaElementary.hw11.test;
+
+public class TestThread implements Runnable{
+
+    @Override
+    public void run(){
+        System.out.println("TestThread: " + ", priority ");
+    }
+}
